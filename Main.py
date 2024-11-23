d6e7a4ad6844405a50ee62d125af314e6ba222b7
Racicley <racicley@gmail.com>
2024-11-23 12:53:38 -0300

    feat: inclusão da funcionalidade de passar o abstract como parametro da instância da classe RecommendArticle

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from RecomendArticle import RecomendArticle
 
 if __name__ == "__main__":
-    ra = RecomendArticle('csv_buscas_unificado_ajustado_final.csv')
-    article_source, articles, index_near, index_futher = ra.print_recommendations_from_strings(0, 5)
+    readed_article = '''Perceived Efficacy of Medical Cannabis in the Treatment of Co-Occurring Health-Related Quality of Life Symptoms,"For persons living with chronic conditions, health-related quality of life (HRQoL) symptoms, such as pain, anxiety, depression, and insomnia, often interact and mutually reinforce one another. There is evidence that medical cannabis (MC) may be efficacious in ameliorating such symptoms and improving HRQoL. As many of these HRQoL symptoms may mutually reinforce one another, we conducted an exploratory study to investigate how MC users perceive the efficacy of MC in addressing co-occurring HRQoL symptoms. We conducted a cross-sectional online survey of persons with a state medical marijuana card in Illinois ('''
+
+    ra = RecomendArticle('csv_buscas_unificado_ajustado_final.csv',  readed_article)
+    article_source, articles, index_near, index_futher = ra.print_recommendations_from_strings(6)
     
     print('Artigo Fonte')
     print(article_source)
--- RecomendArticle.py
+++ RecomendArticle.py
@@ -9,20 +9,19 @@ from ControllerOpenAI import ControllerOpenAI
 
 
 class RecomendArticle:
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, readed_abstract):
         #Get OpenAI Controller instace
         self.openai_ctr = ControllerOpenAI()
-        #Archive base name
-        #dataset_path = dataset_path
         #Load CSV
         df = pd.read_csv(dataset_path)
         #Load abstract as a list from df
         self.abstract_article_list = df['ABSTRACT'].tolist()
+        self.abstract_article_list.insert(0, readed_abstract)
         #Load DOIs as a list from df
         self.doi_article_list = df['DOI'].tolist()
-        # set path to embedding cache
+        #Set path to embedding cache
         self.embedding_cache_path = dataset_path.split('.')[0]+'.pkl'
-        # load the cache if it exists, and save a copy to disk
+        #Load the cache if it exists, and save a copy to disk
         try:
             self.embedding_cache = pd.read_pickle(self.embedding_cache_path)
         except FileNotFoundError:
@@ -30,17 +29,15 @@ class RecomendArticle:
         with open(self.embedding_cache_path, "wb") as embedding_cache_file:
             pickle.dump(self.embedding_cache, embedding_cache_file)
 
-        #return (self.openai_ctr,embedding_cache,embedding_cache_path)
+    def embedding_from_dataset(self, text, model) -> list:
 
-    def embedding_from_dataset(self,texto, modelo) -> list:
-
-        """Retorna o embedding de um texto, usando cache para evitar recomputação."""
-        if (texto, modelo) not in self.embedding_cache:
-            self.embedding_cache[(texto, modelo)] = self.openai_ctr.get_embedding(texto, modelo)
+        """Return text embedding, using cache to avoid recomputing."""
+        if (text, model) not in self.embedding_cache:
+            self.embedding_cache[(text, model)] = self.openai_ctr.get_embedding(text, model)
             with open(self.embedding_cache_path, "wb") as cache_file:
                 pickle.dump(self.embedding_cache, cache_file)
 
-        return self.embedding_cache[(texto, modelo)]
+        return self.embedding_cache[(text, model)]
     
     def print_neighbors_by_distance(
             self,
@@ -53,17 +50,17 @@ class RecomendArticle:
         k_counter = 0
         article_return = []
         for i in indices_of_neighbors:
-            # skip any strings that are identical matches to the starting string
+            #Skip any strings that are identical matches to the starting string
             if query_string == self.abstract_article_list[i]:
                 continue
-            # stop after printing out k articles
+            #Stop after printing out k articles
             if k_counter >= k_neighbors:
                 break
             k_counter += 1
 
             article_return.append(
                 {
-                    'prioridade_artigo': k_counter,
+                    'article_priority': k_counter,
                     'abstract': self.abstract_article_list[i],
                     'distance': distances[i],
                     'doi': self.doi_article_list[i]
@@ -73,30 +70,30 @@ class RecomendArticle:
 
     def print_recommendations_from_strings(
             self,
-            index_of_source_string: int,
             k_nearest_neighbors: int = 1,
             model="text-embedding-3-small",
             ) -> list[int]:
 
         """Print out the k nearest neighbors of a given string."""
-        # get embeddings for all strings
-        embeddings = [self.embedding_from_dataset(string, modelo=model) for string in self.abstract_article_list]
+        #Define index source
+        index_of_source_string = 0
 
-        # get the embedding of the source string
+        #Get embeddings for all strings
+        embeddings = [self.embedding_from_dataset(string, model=model) for string in self.abstract_article_list]
+
+        #Get the embedding of the source string
         query_embedding = embeddings[index_of_source_string]
 
-        # get distances between the source embedding and other embeddings (function from ControllerOpenAI.py)
+        #Get distances between the source embedding and other embeddings (function from ControllerOpenAI.py)
         distances = self.openai_ctr.distances_from_embeddings(query_embedding, embeddings, distance_metric="cosine")
-        #print(type(distances))
-        # get indices of nearest neighbors (function from utils.utils.embeddings_utils.py)
-        indices_of_nearest_neighbors = self.openai_ctr.indices_of_nearest_neighbors_from_distances(distances)
-        #indices_of_nearest_neighbors =  np.argsort(distances)
-        indices_of_furtherest_neighbors = self.openai_ctr.indices_of_furtherest_neighbors_from_distances(distances)
-        #indices_of_furtherest_neighbors = np.argsort(distances)[::-1]
 
-        # print out source string
+        #Get indices of nearest neighbors (function from utils.utils.embeddings_utils.py)
+        indices_of_nearest_neighbors = self.openai_ctr.indices_of_nearest_neighbors_from_distances(distances)
+
+        indices_of_furtherest_neighbors = self.openai_ctr.indices_of_furtherest_neighbors_from_distances(distances)
+
+        #Get query_string from abstract
         query_string = self.abstract_article_list[index_of_source_string]
-        #print(f"Source string: {query_string}")
         
         articles = self.print_neighbors_by_distance(distances=distances,indices_of_neighbors=indices_of_nearest_neighbors,k_neighbors=k_nearest_neighbors, query_string=query_string)
             
